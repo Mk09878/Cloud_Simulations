a582e1491786ef9999d0a30746e1b255d76cfac1
Mihir Kelkar <mihirk107@gmail.com>
2020-09-27 15:16:27 -0500

    Updated IaaS_Inputs file

--- src/main/scala/Utils/IaaS_Inputs.scala
+++ src/main/scala/Utils/IaaS_Inputs.scala
@@ -12,22 +12,35 @@ class IaaS_Inputs {
   logger.info("\n1. Windows\n2. Linux\n3. macOS")
   val os: String = scala.io.StdIn.readLine()
 
-  logger.info("Enter number of VMs (Less than or equal to 4)")
+  logger.info("RANGE OF PARAMETERS ARE PROVIDED. DEVIATING FROM THESE MIGHT LEAD TO INFINITE LOOPS")
+  logger.info("Enter number of VMs (Between 2 and 4 inclusive)")
   val number: Int = scala.io.StdIn.readLine().toInt
+  if(!(2 to 4 contains number))
+    logger.warn("You violated the expected range")
 
-  logger.info("Enter VM Mips (Less than or equal to 500)")
+  logger.info("Enter VM Mips (Between 300 and 1000 inclusive)")
   val mips: Int = scala.io.StdIn.readLine().toInt
+  if(!(300 to 1000 contains mips))
+    logger.warn("You violated the expected range")
 
-  logger.info("Enter number of Pes for VM (Less than or equal to 4)")
+  logger.info("Enter number of Pes for VM (Between 3 and 4 inclusive)")
   val pes: Int = scala.io.StdIn.readLine().toInt
+  if(!(3 to 4 contains pes))
+    logger.warn("You violated the expected range")
 
-  logger.info("Enter VM Ram (Less than or equal to 10000)")
+  logger.info("Enter VM Ram (Between 5000 and 10000 inclusive)")
   val ram: Int = scala.io.StdIn.readLine().toInt
+  if(!(5000 to 10000 contains ram))
+    logger.warn("You violated the expected range")
 
-  logger.info("Enter VM Bandwidth (Less than or equal to 10000)")
+  logger.info("Enter VM Bandwidth (Between 5000 and 10000 inclusive)")
   val bw: Int = scala.io.StdIn.readLine().toInt
+  if(!(5000 to 10000 contains bw))
+    logger.warn("You violated the expected range")
 
-  logger.info("Enter VM Size (Less than or equal to 100000)")
+  logger.info("Enter VM Size (Between 5000 and 100000 inclusive)")
   val size: Int = scala.io.StdIn.readLine().toInt
+  if(!(5000 to 100000 contains size))
+    logger.warn("You violated the expected range")
 
 }
